361a9b613258788380e4951634faa5d4952c773b
SimLacombe <simon.lacombe@ens-lyon.fr>
2024-02-28 12:26:51 +0100

    add PNA data

--- src/open_datafiles/Occitanie-LPO.R
+++ src/open_datafiles/Occitanie-LPO.R
@@ -1,6 +1,7 @@
 require(tidyverse, lubridate)
 
 dat.filename <- "data/PNA-data/Occitanie-LPOoccitanie/extraction SFEPM Loutre 1980-2022.xlsx"
+dat2.filename <- "data/PNA-data/Occitanie-LPOoccitanie/PNAdata.csv"
 
 dat <- readxl::read_xlsx(dat.filename)
 names(dat) <- names(readxl::read_xlsx(dat.filename, n_max = 1))
@@ -15,6 +16,14 @@ dat <- dat %>%
   rename(lon.l93 = `X Lambert93 [m]`,
          lat.l93 = `Y Lambert93 [m]`,
          grid.cell = Maille)
-
 dat <- dat %>%
   select(data.provider, PNA.protocole, year, date, loc, lon.l93, lat.l93, grid.cell, presence)
+
+dat2 <- read.csv(dat2.filename, sep = ";", header = T) %>%
+  mutate(PNA.protocole = TRUE,
+         data.provider = "LPO",
+         lon.l93 = NA,
+         lat.l93 = NA) %>%
+  select(data.provider, PNA.protocole, year, date, loc, lon.l93, lat.l93, grid.cell, presence)
+
+dat <- rbind(dat, dat2)
