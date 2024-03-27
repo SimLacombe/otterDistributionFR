8dcf7dd5e3f40e86541565a63f1cea9a0d7a8b81
Simon LACOMBE <lacombe@newcefe.newage.fr>
2024-03-27 15:20:50 +0100

    gather information about the different protocoles from the remarque section

--- src/open_datafiles/PDL-Anjou.R
+++ src/open_datafiles/PDL-Anjou.R
@@ -1,24 +1,30 @@
-require(tidyverse, lubridate)
+library(tidyverse)
+library(lubridate)
 
 dat.filename <- "data/PNA-data/PaysdelaLoire-LPOanjou/Copie de Export_données_loutre_SFEPM_CEFE_LPO 24_07_2023.xlsx"
+protocoles <- c("COLL", "CT", "KAYAK", "UICN", "OA", "ENS", "PNR", "N2000", "SBO", "ONCFS", "CLCT", "OPP")
 
 dat <- readxl::read_xlsx(dat.filename)
 
 dat <- dat %>% 
   mutate(Remarques = toupper(Remarques),
-         tr_len = as.numeric(gsub("\\D", "", str_extract(Remarques, "\\d\\d\\d\\dM"))),
-         tr_len = ifelse(is.na(tr_len), as.numeric(gsub("\\D", "", str_extract(Remarques, "\\d\\d\\d\\d M"))), tr_len),
-         tr_len = ifelse(is.na(tr_len), as.numeric(gsub("\\D", "", str_extract(Remarques, "\\d\\d\\d M"))), tr_len),
-         tr_len = ifelse(is.na(tr_len), as.numeric(gsub("\\D", "", str_extract(Remarques, "\\d\\d\\dM"))), tr_len),
-         tr_len = ifelse(is.na(tr_len), as.numeric(gsub("\\D", "", str_extract(Remarques, "\\d\\dKM")))*100, tr_len),
-         tr_len = ifelse(is.na(tr_len), as.numeric(gsub("\\D", "", str_extract(Remarques, "\\dKM")))*10000, tr_len),
-         tr_len = ifelse(is.na(tr_len), as.numeric(gsub("\\D", "", str_extract(Remarques, "\\d\\d KM")))*1000, tr_len),
-         tr_len = ifelse(is.na(tr_len), as.numeric(gsub("\\D", "", str_extract(Remarques, "\\d KM")))*1000, tr_len))%>%
-  mutate(PA = grepl("PNA", Remarques)|grepl("PRA", Remarques)|grepl("UICN", Remarques)|grepl("POINT", Remarques)|grepl("MAILLE", Remarques),
-         PA = PA|tr_len >= 300,
-         PA = ifelse(is.na(PA), FALSE, PA),
-         PA.protocole = ifelse(PA, "transect", NA),
-         collision = grepl("ÉCRASÉ", Remarques)|grepl("CADAVRE", Remarques))%>%
+         COLL = grepl("ÉCRASÉ", Remarques)|grepl("CADAVRE", Remarques),
+         CT = grepl("PIÈGE-PHOTO", Remarques)|grepl("PIÈGE PHOTO", Remarques),
+         KAYAK = grepl("CANOÉ", Remarques)|grepl("CANOË", Remarques)|grepl("KAYAK", Remarques),
+         UICN = grepl("PNA", Remarques)|grepl("PRA", Remarques)|grepl("UICN", Remarques)|grepl("IUCN", Remarques)|grepl("POINT N", Remarques)|
+           grepl("POINT \\d\\d", Remarques)|grepl("POINT\\d", Remarques)|grepl("POINT DE SUIVI", Remarques)|grepl("MAILLE", Remarques),
+         OA = grepl(" OA", Remarques)|grepl("OUVRAGE", Remarques),
+         ENS = grepl("ENS", Remarques)&(grepl("COUASNON", Remarques)|grepl("BAUGÉ", Remarques)),
+         PNR = grepl("PNR", Remarques),
+         N2000 = grepl("N2000", Remarques)|grepl("NATURA 2000", Remarques),
+         SBO = grepl("PROSPECTION", Remarques)&(grepl("OUDON", Remarques)|grepl("SBO", Remarques)),
+         ONCFS = grepl("PROSPECTION", Remarques)&grepl("ONCFS", Remarques),
+         CLCT = grepl("COLLECTIVE", Remarques)&grepl("PROSPECTION", Remarques),
+         OPP = !CT&!UICN&!KAYAK&!OA&!ENS&!PNR&!N2000&!SBO&!ONCFS&!COLL)
+
+
+dat <- dat %>% 
+  pivot_longer(cols = protocoles, names_to = "protocole", values_to = "TF") %>%
   mutate(date = as.Date(Date),
          year = year(date),
          presence = sign(Nombre),
@@ -27,5 +33,11 @@ dat <- dat %>%
   rename(lon.l93 = `X Lambert93 [m]`,
          lat.l93 = `Y Lambert93 [m]`,
          grid.cell = Maille) %>%
+  group_by(Réf, data.provider, year, date, loc, lon.l93, lat.l93, grid.cell, presence) %>%
+  summarise(protocole = protocole[TF][1]) %>%
+  mutate(PA = (protocole != "OPP"),
+         PA.protocole = ifelse(protocole %in% c("UICN", "KAYAK"), "transect", ifelse(protocole %in% c("OPP", "CT", "COLL"), "point", NA)),
+         collision = protocole == "COLL") %>%
+  ungroup %>%
   select(data.provider, PA, PA.protocole, collision, year, date, loc, lon.l93, lat.l93, grid.cell, presence)
 
