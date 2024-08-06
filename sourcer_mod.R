8c4e09f33fd7e9cb152202f4119c0b0e1222ecc0
SimLacombe <simon.lacombe0@gmail.com>
2024-08-06 19:53:56 +0200

    3 subregions

--- sourcer_mod.R
+++ sourcer_mod.R
@@ -47,51 +47,51 @@ effort_full <- readRDS(effort.filename)
 
 ### 1. Full country ------------------------------------------------------------
 
-REGIONS <- c("72", "83", "25", "26", "53",
-             "24", "43", "23", "91",
-             "74", "73", "52",
-             "54", "93", "82")
+# REGIONS <- c("72", "83", "25", "26", "53",
+#              "24", "43", "23", "91",
+#              "74", "73", "52", "22", "11", "31",
+#              "54", "93", "82", "42", "21", "41")
+# 
+# source("src/fit_JAGS.R")
+# 
+# outpath <- paste0("out/","Mod_full_", format(Sys.time(),"%Y%m%d_%H%M%S"), ".RData")
+# 
+# save.image(file=outpath)
+# 
+# rm(list = setdiff(ls(), c("otterDat_full", "L93_grid_full", "effort_full", "CFdata_full", "my_inits", "jagsPar")))
+
+### 2. North-West --------------------------------------------------------------
+
+REGIONS <- c("52", "53"," 24", "25")
 
 source("src/fit_JAGS.R")
 
-outpath <- paste0("out/","Mod_full_", format(Sys.time(),"%Y%m%d_%H%M%S"), ".RData")
+outpath <- paste0("out/","Mod_NO_", format(Sys.time(),"%Y%m%d_%H%M%S"), ".RData")
 
 save.image(file=outpath)
 
 rm(list = setdiff(ls(), c("otterDat_full", "L93_grid_full", "effort_full", "CFdata_full", "my_inits", "jagsPar")))
 
-### 2. North-West --------------------------------------------------------------
-
-# REGIONS <- c("52", "53"," 24", "25")
-# 
-# source("src/fit_JAGS.R")
-# 
-# outpath <- paste0("out/","Mod_NO_", format(Sys.time(),"%Y%m%d_%H%M%S"), ".RData")
-# 
-# save.image(file=outpath)
-# 
-# rm(list = setdiff(ls(), c("otterDat_full", "L93_grid_full", "effort_full", "CFdata_full", "my_inits", "jagsPar")))
-
 ### 3. South-East --------------------------------------------------------------
-# 
-# REGIONS <- c("83", "91","73", "93", "82")
-# 
-# source("src/fit_JAGS.R")
-# 
-# outpath <- paste0("out/","Mod_SE_", format(Sys.time(),"%Y%m%d_%H%M%S"), ".RData")
-# 
-# save.image(file=outpath)
-# 
-# rm(list = setdiff(ls(), c("otterDat_full", "L93_grid_full", "effort_full", "CFdata_full", "my_inits", "jagsPar")))
-# 
+
+REGIONS <- c("83", "91","73", "93", "82")
+
+source("src/fit_JAGS.R")
+
+outpath <- paste0("out/","Mod_SE_", format(Sys.time(),"%Y%m%d_%H%M%S"), ".RData")
+
+save.image(file=outpath)
+
+rm(list = setdiff(ls(), c("otterDat_full", "L93_grid_full", "effort_full", "CFdata_full", "my_inits", "jagsPar")))
+
 # ### 4. East --------------------------------------------------------------
-# 
-# REGIONS <- c("74", "83", "26", "24", "43", "82")
-# 
-# source("src/fit_JAGS.R")
-# 
-# outpath <- paste0("out/","Mod_E_", format(Sys.time(),"%Y%m%d_%H%M%S"), ".RData")
-# 
-# save.image(file=outpath)
-# 
-# rm(list = setdiff(ls(), c("otterDat_full", "L93_grid_full", "effort_full", "CFdata_full", "my_inits", "jagsPar")))
+
+REGIONS <- c("74", "83", "26", "24", "43", "82")
+
+source("src/fit_JAGS.R")
+
+outpath <- paste0("out/","Mod_E_", format(Sys.time(),"%Y%m%d_%H%M%S"), ".RData")
+
+save.image(file=outpath)
+
+rm(list = setdiff(ls(), c("otterDat_full", "L93_grid_full", "effort_full", "CFdata_full", "my_inits", "jagsPar")))
