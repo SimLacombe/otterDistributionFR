7acfd54a991e768d8fb4cc380c32a87db1c09a21
SimLacombe <simon.lacombe0@gmail.com>
2024-10-16 14:14:37 +0200

    use the CEFE partition !!

--- run_model.sh
+++ run_model.sh
@@ -9,7 +9,7 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks=4  
 #SBATCH --mem-per-cpu=12G
-#SBATCH --partition=defq
+#SBATCH --partition=cefe
 
 ### Email
 #SBATCH --mail-user=simon.lacombe@cefe.cnrs.fr        # Change user for e-mail notification
